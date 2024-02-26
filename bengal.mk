4682ac0826099bfe7268121ecfc8174abb7710bb
darknius20 <darknius21@gmail.com>
2024-02-26 23:14:16 +0000

    Revert "sm6115-common: parts: Import XiaomiParts and sepolicy rules from old tree"
    This reverts commit 2cf7f8ff0d3e0d60b3c18ca8cfc140a96badea0f.

--- bengal.mk
+++ bengal.mk
@@ -147,13 +147,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
-# Device Settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-parts.xml
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
